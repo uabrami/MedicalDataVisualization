753352a6d0ab1fd4c0669e394741704ba47f62f7
Fred Zirdung <fred@hackreactor.com>
2019-05-03 05:17:18 -0700

    (init)

---
+++ medical-data-viz/server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+// ----------------------------------------------------
+// TODO: Fill in the request handler for this endpoint!
+// ----------------------------------------------------
+app.get('/api/heartFailures', (req, res) => { 
+  // ----------------------------------------------------
+  // TODO: Fill in the request handler for this endpoint!
+  // ----------------------------------------------------
+  
+    // -----------------------------------------------------
+    // TODO: Send a request to the HospitalCompare API here!
+    // -----------------------------------------------------
+
+    // -----------------------------------------------------
+    // TODO: Do all data processing/wrangling/munging here!
+    // -----------------------------------------------------
+
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
